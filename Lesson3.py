0e933d3a5d3857535595770fcbb8f9da8cb53460
Daryna <ddaryna7@gmail.com>
2022-12-09 17:42:18 +0200

    Lesson 3. Teority

---
+++ Lesson3.py
@@ -0,0 +1,27 @@
+class Human:
+    def __init__(self , name = "Human" ):
+        self.name = name
+class Auto:
+    def __init__(self , brand):
+        self.brand = brand
+        self.passenger = []
+    def add_passenger(self, human):
+        self.passengers.append(human)
+    def print_passengers_names(self):
+        if self.passengers != []:
+            print(f"Names of {self.brand} passengers:")
+            for passenger in  self.passengers:
+                print(passenger.name)
+        else:
+            print(f"There are no passengers in {self.brand}")
+Max = Human("Maxim")
+Sviatoslav = Human ("Sviatoslav")
+Maxim = Human ("Maxim")
+car = Auto("Mercedes")
+car.add_passengers(Max)
+car.add_passengers(Sviatoslav)
+car.add_passengers(Maxim)
+car.print_passengers_names()
+car1 = Auto("Tesla")
+car1.print_passengers_names()
+print()
